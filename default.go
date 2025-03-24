c07a4ab7d20fe433534068499d48dd5c741ff5e5
Axel Etcheverry <axel@etcheverry.biz>
2025-03-24 21:54:41 +0100

    fix: lint

--- default.go
+++ default.go
@@ -1,13 +1,16 @@
 // Package reggen generates text based on regex definitions
 package reggen
 
+import "fmt"
+
 const defaultLimit = 258
 
-// Generate string by regexp
+// Generate string by regexp.
 func Generate(regex string) (string, error) {
 	g, err := NewGenerator(regex)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("failed to create generator: %w", err)
 	}
+
 	return g.Generate(defaultLimit), nil
 }
